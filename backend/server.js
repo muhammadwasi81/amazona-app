a27a07ebc769be122d22a554c7e2d7f3dd1a4524
muhammad wasi81 <wasiarain81@gmail.com>
2021-01-23 16:01:06 +0500

    very first commit

--- backend/server.js
+++ backend/server.js
@@ -36,3 +36,16 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
+
+
+
+// serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*',(req, res) => {
+   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')
+   )});
+};
+const path = require('path');
